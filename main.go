328e4a1e8cc5e225c8c2203e2bf2f28781da013e
Vdtry-06 <tunhoipro0306@gmail.com>
2025-04-14 00:45:38 +0700

    Create API

--- main.go
+++ main.go
@@ -49,6 +49,26 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	// CRUD: Create, Read, Update, Delete
+	// POST: /v1/items (create a new item)
+	// GET: /v1/items (list items) /v1/items/items?page=1 || /v1/items?cursor=fdsfsdk
+	// GET: /v1/items/:id (get item detail by id)
+	// PUT || PATCH: /v1/items/:id (update a item by id)
+	// DELETE: /v1/items/:id (delete a item by id)
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("")
+			items.GET("")
+			items.GET("/:id")
+			items.PATCH("/:id")
+			items.DELETE("/:id")
+		}
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 		"message": item,

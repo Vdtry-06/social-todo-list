ae476134cd7c5d4fc7f3adb1ca4581bb73b1059e
Vdtry-06 <tunhoipro0306@gmail.com>
2025-04-16 05:21:25 +0700

    format code, add enum

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,11 +15,55 @@ import (
 	"gorm.io/gorm"
 )
 
+type ItemStatus int
+
+const (
+	ItemStatusDoing ItemStatus = iota
+	ItemStatusDone
+	ItemStatusDeleted
+)
+
+var allItemStatuses = [3]string{"Doing", "Done", "Deleted"}
+
+func (item ItemStatus) String() string {
+	return allItemStatuses[item]
+}
+
+func parseStr2ItemStatus(s string) (ItemStatus, error) {
+	for i := range allItemStatuses {
+		if allItemStatuses[i] == s {
+			return ItemStatus(i), nil
+		}
+	}
+	return ItemStatus(0), errors.New(fmt.Sprintf("invalid item status: %s", s))
+}
+
+func (item * ItemStatus) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprintf("failed to scan data from sql: %s", value))
+	}
+
+	v, err := parseStr2ItemStatus(string(bytes))
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("failed to parse item status: %s", value))
+	}
+
+	*item = v
+
+	return	nil
+}
+
+func (item *ItemStatus) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%s\"", item.String())), nil
+}
+
 type TodoItem struct {
 	Id 			int 		`json:"id" gorm:"column:id"`
 	Title 		string 		`json:"title" gorm:"column:title"`
 	Description string 		`json:"description" gorm:"column:description"`
-	Status 		string 		`json:"status" gorm:"column:status"`
+	Status 		*ItemStatus	`json:"status" gorm:"column:status"`
 	CreatedAt 	*time.Time 	`json:"created_at" gorm:"column:created_at"`
 	UpdatedAt 	*time.Time 	`json:"updated_at,omitempty" gorm:"column:updated_at"`
 
